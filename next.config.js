8b50fb43af97984b8b7ce92c0eca7a59ee9cdcac
Au_Il <38571339+AustIlphukir@users.noreply.github.com>
2025-09-12 00:14:33 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
       {
         source: "/architecture",
         destination: "http://localhost:8080",
+        // destination: "https://d1rmg6jcg0tsw.cloudfront.net/viewer/3060a252-e667-48f3-ad33-d4d3fb06ce1a",
         permanent: false, // set to true if this will never change
       },
     ]
@@ -17,3 +18,5 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+
